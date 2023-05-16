5ce859ef472950911a115b9927d06a0f1498503a
sagiv bonali <sagivb13@gmail.com>
2023-05-16 13:06:03 +0300

    Change mock data

--- lib/bloc/article_bloc.dart
+++ lib/bloc/article_bloc.dart
@@ -12,8 +12,10 @@ class ArticleBloc extends Bloc<ArticleEvent, ArticleState> {
       await Future<void>.delayed(const Duration(seconds: 1));
       List<ArticleModel> bb = List<ArticleModel>.empty(growable: true);
       ArticleModel aa = ArticleModel(
-          title: 'title',
-          description: 'description',
+          title:
+              'Elon Musk reportedly tells Tesla staff he must approve all hiring',
+          description:
+              'Tesla Chief Executive Elon Musk has said that the company can make no new hires unless he personally approves them, including contractors, the Information reported on Monday, citing an email to staff. According to the report, Musk told executives to send him …',
           author: 'author',
           url: 'url');
       bb.add(aa);
