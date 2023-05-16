5042615e9d3198f610ea5229ac8d5e122b38d078
sagiv bonali <sagivb13@gmail.com>
2023-05-16 13:06:24 +0300

    Show all values on array with article_card widget

--- lib/view/page/articles.dart
+++ lib/view/page/articles.dart
@@ -1,4 +1,5 @@
 import 'package:articles/bloc/article_bloc.dart';
+import 'package:articles/view/widget/article_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -22,7 +23,11 @@ class _ArticleState extends State<Article> {
           );
         }
         if (state is ArticleLoadedState) {
-          return Text('${state.articles.length} :) ');
+          return ListView.builder(
+              itemCount: state.articles.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ArticleCard(article: state.articles[index]);
+              });
         }
 
         return const Text('End of page');
